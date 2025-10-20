ad982a5ed2a0d5a5a4bf9e813c893d73c9654cff
Egbaiyelo <moteniolaegbaiyelo@trentu.ca>
2025-10-19 22:17:51 -0400

    update: separating bundable methods

--- extension/content.js
+++ extension/content.js
@@ -1,6 +1,7 @@
 
 import { createAccountHelper, createHomeLink } from "./files/dom";
 import { signIn, createAccount } from "./files/auth"
+import { addSite } from "./files/addsite";
 
 // Add site
 // Login
@@ -19,47 +20,6 @@ import { signIn, createAccount } from "./files/auth"
 
 const siteURL = window.location.href;
 
-// Adds site to the watch list
-function addSite() {
-
-    // Company name in format like 
-    // bmo.wd3.myworkdayjobs
-    const companyName = window.location.hostname.split('.')[0];
-
-    const segments = siteURL.split('/');
-    const baseURL = segments.slice(0, 5).join('/');
-
-    // Save to local
-    // Redundant storage, given some may not have the app or maybe the app isnt working, it at least keeps them 
-    // in local storage first
-    //- not used sync yet but might have addsites
-    chrome.storage.sync.get("companySites", function (result) {
-        const companySites = result.companySites || {};
-        console.log("result, companysites", result, companySites);
-
-        if (!companySites[companyName]) {
-            chrome.runtime.sendMessage({ action: "addSite", data: { companyName, url: baseURL } });
-        }
-
-        // Shouldn't have duplicates but just checking
-        // If the company name exists but doesnt have the same data as the baseURL
-        if (companySites[companyName] && companySites[companyName] != baseURL) {
-            chrome.runtime.sendMessage({ action: "siteChange", data: { companyName, url: baseURL, oldUrl: companySites[companyName] } });
-        }
-        // Update or insert
-        companySites[companyName] = baseURL;
-
-        chrome.storage.sync.set({ companySites }, function () {
-            console.log(`Saved ${companyName}: ${baseURL}`);
-        });
-
-        // different protocol if site changes
-        // chrome.runtime.sendMessage({ action: "addSite", data: { companyName, url: baseURL } });
-    });
-
-    // Example full format
-    // "https://bmo.wd3.myworkdayjobs.com/en-US/External/userHome"
-}
 
 // siteStatus();
 
--- extension/files/form.js
+++ extension/files/form.js
@@ -1,5 +1,18 @@
 
 
 export function FillLanguage() {
-    
+    const lang_section = document.querySelector('#Languages-section');
+    const add_button = lang_section.querySelector('[data-automation-id="add-button"]');
+
+    add_button.click();
+
+}
+
+
+export function FillSkills(skills) {
+    const skills_section = document.querySelector('#skills--skills');
+
+    for (const skill in skills) {
+        skills_section.textContent = skill;
+    }
 }
